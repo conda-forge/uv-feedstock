2297e924a3df84cd9efb64b742833abf6541b08a
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-09-24 17:27:42 -0500

    try without extra -fno-plt on linux-ppc64le

--- recipe/build-uv.sh
+++ recipe/build-uv.sh
@@ -17,10 +17,10 @@ fi
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 
 # see https://github.com/conda-forge/uv-feedstock/pull/202#issuecomment-2890816026
-if [[ "${target_platform}" == "linux-ppc64le" ]]; then
-  export CFLAGS="${CFLAGS//-fno-plt/}"
-  export CXXFLAGS="${CXXFLAGS//-fno-plt/}"
-fi
+# if [[ "${target_platform}" == "linux-ppc64le" ]]; then
+#   export CFLAGS="${CFLAGS//-fno-plt/}"
+#   export CXXFLAGS="${CXXFLAGS//-fno-plt/}"
+# fi
 
 cd crates/uv
 
