e0030c95e157491842ead0a63b682136adbcd9ab
Nicholas Bollweg <nick.bollweg@gmail.com>
2026-04-28 08:43:20 -0500

    fix rust version check

--- recipe/build-uv.sh
+++ recipe/build-uv.sh
@@ -4,7 +4,7 @@ set -eux
 echo "ensuring rust-version in Cargo.toml:#/workspace/package/rust-version is ${CBC_RUST_VERSION}"
 CARGO_TOML_RUST_VERSION=$(grep -iE "rust-version = \".*\"" Cargo.toml)
 
-if [[ "${CARGO_TOML_RUST_VERSION}" ~= "rust-version = \"${CBC_RUST_VERSION}.*\"" ]] ;
+if [[ $(echo "${CARGO_TOML_RUST_VERSION}" | grep -iE "rust-version = \"${CBC_RUST_VERSION}.*\"") ]] ;
 then
   echo "OK rust version in Cargo.toml and conda_build_config.yaml agree: ${CBC_RUST_VERSION}"
 else
