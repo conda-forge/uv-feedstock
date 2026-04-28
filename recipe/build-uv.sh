261916763b18c7d0a4ecec295251b3ceeebc7a45
Nicholas Bollweg <nick.bollweg@gmail.com>
2026-04-28 08:23:01 -0500

    try any old rust 1.93.*

--- recipe/build-uv.sh
+++ recipe/build-uv.sh
@@ -4,7 +4,7 @@ set -eux
 echo "ensuring rust-version in Cargo.toml:#/workspace/package/rust-version is ${CBC_RUST_VERSION}"
 CARGO_TOML_RUST_VERSION=$(grep -iE "rust-version = \".*\"" Cargo.toml)
 
-if [[ "${CARGO_TOML_RUST_VERSION}" == "rust-version = \"${CBC_RUST_VERSION}\"" ]] ;
+if [[ "${CARGO_TOML_RUST_VERSION}" ~= "rust-version = \"${CBC_RUST_VERSION}.*\"" ]] ;
 then
   echo "OK rust version in Cargo.toml and conda_build_config.yaml agree: ${CBC_RUST_VERSION}"
 else
@@ -13,14 +13,6 @@ else
   echo "    to match ${CARGO_TOML_RUST_VERSION}"
   exit 2
 fi
-
-export CARGO_PROFILE_RELEASE_STRIP=symbols
-
-# see https://github.com/conda-forge/uv-feedstock/pull/202#issuecomment-2890816026
-# if [[ "${target_platform}" == "linux-ppc64le" ]]; then
-#   export CFLAGS="${CFLAGS//-fno-plt/}"
-#   export CXXFLAGS="${CXXFLAGS//-fno-plt/}"
-# fi
 
 cd crates/uv
 
