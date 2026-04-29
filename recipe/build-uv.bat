320f64f3ed0bbcf3125ad790d8550a8c19a73e4f
Nicholas Bollweg <nick.bollweg@gmail.com>
2026-04-29 08:57:37 -0500

    win completions, try more ppc64le linker stuff

--- recipe/build-uv.bat
+++ recipe/build-uv.bat
@@ -10,8 +10,20 @@ cargo install ^
     --root "%LIBRARY_PREFIX%" ^
     || exit 1
 
-
 cargo-bundle-licenses ^
     --format yaml ^
     --output "%SRC_DIR%\THIRDPARTY.yml" ^
     || exit 3
+
+md ^
+    "%LIBRARY_PREFIX%\share\bash-completion\completions" ^
+    "%LIBRARY_PREFIX%\share\fish\vendor_completions.d" ^
+    "%LIBRARY_PREFIX%\share\zsh\site-functions" ^
+    || exit 4
+
+"%LIBRARY_BIN%\uv.exe" completion bash > "%LIBRARY_PREFIX%\share\bash-completion\completions\uv" ^
+    || exit 5
+"%LIBRARY_BIN%\uv.exe" completion fish > "%LIBRARY_PREFIX%\share\fish\vendor_completions.d\uv.fish" ^
+    || exit 6
+"%LIBRARY_BIN%\uv.exe" completion zsh  > "%LIBRARY_PREFIX%\share\zsh\site-functions\uv" ^
+    || exit 7
